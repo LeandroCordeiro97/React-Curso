53c217069defeb08c20eee75c7fe9ec247108a68
LeandroCordeiro97 <leandrocordeiro97@live.com.pt>
2023-06-19 18:28:04 +0100

    update

---
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -0,0 +1,79 @@
+import './App.css';
+
+import { useState, useEffect } from "react";
+const url ="http://localhost:3000/products"
+
+function App() {
+  const [products,setProducts] = useState([]);
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+
+  //1- Resgatar dados
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const res = await fetch(url);
+      const data = await res.json();
+      setProducts(data);
+      
+    }
+    fetchData();
+  }, []); // Or [] if effect doesn't need props or state
+
+  //2-ADD Products
+  const handleSubmit = async (e) =>{
+    e.preventDefault()
+    const product ={
+      name,
+      price,
+    };
+
+  const res = await fetch(url, {
+    method:"POST",
+    headers:{
+      "Content-Type": "application/json"
+    },
+    body:JSON.stringify(product)
+  });
+
+  //3 - Carregamento Dinâmico
+  const addedProduct = await res.json()
+  setProducts((prevProducts)=>[...prevProducts,addedProduct]);
+
+  setName("")
+  setPrice("")
+  
+  }
+  
+return (
+    <div className="App">
+      <h1>Lista De Produtos</h1>
+      <ul>
+        {products.map((product) =>(
+          <li key={product.id}>{product.name} - €{product.price}</li>
+        ))}
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" 
+            value={name} 
+            name="name" 
+            onChange={(e) => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço:
+            <input type="number" 
+            value={price} 
+            name="price" onChange={(e) => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" 
+          value="Criar" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
