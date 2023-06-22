cf55b1e38f57a5ed022bf09c67f7ac836d2b0556
LeandroCordeiro97 <leandrocordeiro97@live.com.pt>
2023-06-22 17:29:19 +0100

    update

--- 8_React_Router/reactrouter/src/Pages/Product.js
+++ 8_React_Router/reactrouter/src/Pages/Product.js
@@ -1,11 +1,25 @@
 import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/UseFetch";
 
 const Product= () => {
 {/*4- Rota Dinmaica*/}
 const {id} = useParams();
-  return (
+
+//5-Carregamento Dado Individual
+const url = "http://localhost:3000/products/" + id
+const {data:product,loading,error} = useFetch(url);
+console.log(product)
+  
+
+return (
    <div>
     <p>ID do porduto:{id}</p>
+    {error && <p>Ocorreu um erro..</p>}
+    {loading && <p>Carregando...</p>}
+    {product && (<div>
+      <h1>{product.name}</h1>
+      <p>€{product.price}</p>
+    </div>)}
     </div>
   )
 }
